1ef16d351f733d1656f7b86b85b865913383c7dd
nsdaveta <100615660+nsdaveta@users.noreply.github.com>
2026-03-12 20:08:43 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000/todos',
+    baseURL: 'https://to-do-server-1yuc.onrender.com/todos',
 });
 
 // Add a request interceptor to automatically add the auth token from localStorage
