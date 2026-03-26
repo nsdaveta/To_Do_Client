3dcd7215b0a10b2a58373ab7803d5991f7052a93
nsdaveta <100615660+nsdaveta@users.noreply.github.com>
2026-03-26 19:11:30 +0530

    Some more PWA fixes............

--- public/verify_png.mjs
+++
@@ -1,3 +0,0 @@
-import fs from 'fs';
-const buffer = fs.readFileSync('c:/Users/nsdav/OneDrive/Desktop/MERN STACK/To_Do_List/To_Do_Client/public/logo-pwa.png');
-console.log('Hex Header:', buffer.toString('hex', 0, 8));
