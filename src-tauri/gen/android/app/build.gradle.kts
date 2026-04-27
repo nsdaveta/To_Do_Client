0a845b565abb5127f841bfe7526e5875a6ffd857
nsdaveta <100615660+nsdaveta@users.noreply.github.com>
2026-04-27 11:21:15 +0530

    Sync before build (automated)

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -15,10 +15,10 @@ val tauriProperties = Properties().apply {
 
 android {
     compileSdk = 36
-    namespace = "com.todoapp.app"
+    namespace = "com.todoapp.tasks"
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
-        applicationId = "com.todoapp.app"
+        applicationId = "com.todoapp.tasks"
         minSdk = 24
         targetSdk = 36
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
--- src-tauri/gen/android/app/src/main/java/com/todoapp/tasks/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/todoapp/tasks/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.todoapp.app
+package com.todoapp.tasks
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
